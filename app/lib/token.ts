103e5be418b089e763192a2606c68bfabd612a93
fliagoldvargas-hash <fliagoldvargas@gmail.com>
2026-07-29 20:44:54 -0300

    Update token.ts

--- app/lib/token.ts
+++ app/lib/token.ts
@@ -1,5 +1,5 @@
-const GM_TOKEN_MINT = "";
-const GM_PUMPFUN_URL = "";
+const GM_TOKEN_MINT = "CBNjVBxK2neCEwrbjHYYJ1Fq6kKwoMrKYVsu9PFZpump";
+const GM_PUMPFUN_URL = "https://pump.fun/coin/CBNjVBxK2neCEwrbjHYYJ1Fq6kKwoMrKYVsu9PFZpump";
 const GM_LAUNCH_AT = "";
 const GM_X_URL = "https://x.com/Gmswap_";
 const GM_TELEGRAM_URL = "";
